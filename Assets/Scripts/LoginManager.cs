23899487cdbf4c7490603c8889b90fd8a7c7145a
sylvain2 <sylvain.aimoz@2and2.com.au>
2016-01-12 16:36:20 +1100

    registration works

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -29,7 +29,10 @@ public class LoginManager : MonoBehaviour {
 	public GameObject loginGO;
 
 	public GameObject sexGO;
+	public GameObject maleGO;
+	public GameObject femaleGO;
 	public GameObject dobGO;
+
 	public GameObject firstnameGO;
 	public GameObject lastnameGO;
 	public GameObject schoolGO;
@@ -128,27 +131,84 @@ public class LoginManager : MonoBehaviour {
 	}
 
 	public void OnRegisterBtnClick(){
+		
 		if(newPasswordGO.GetComponent<InputField>().text != passwordConfirmationGO.GetComponent<InputField>().text){
 			DisplayScreen(Feedback, true);
 			return;
 		}
 
 		PopulateRegistration();
+//		GenerateForm();
+
+		jsonString = JsonUtility.ToJson(newRegisterDetails);
+		DisplayScreen(Feedback, false);
+		
+		StartCoroutine(Register());
+	}
+
+	IEnumerator Register () {
+		if(debugLogin)
+			Debug.Log("Registering");
+
+
+		//Post need to be provided with headers
+		Dictionary<string, string> headers = new Dictionary<string, string>();
+		headers.Add("Content-Type", "application/json");
+		headers.Add("Accept", "application/json");
+
+		System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+		if(debugLogin)
+			Debug.Log("JSON created from form" + jsonString);
+
+		byte[] json = encoding.GetBytes(jsonString);
+
+		WWW www = new WWW(APIRegistration, json, headers);
+		yield return www;
+
+		string errorCode = "400";
+		if(www.error == null)
+			errorCode = "201";
+		else
+			if(www.error.Length >= 3)
+				errorCode = www.error.Substring(0,3);
+		
+		if(Debug.isDebugBuild || debugLogin)
+			Debug.Log("Answer from " + APIRegistration + ": " + errorCode + www.text);
+		
+		if (errorCode == "200") {
+			RegisterGO.GetComponent<Image>().color = Color.green;
+			CreateUserFromAnswer(www.text);
+				
+		} else {
+		
+			DisplayScreen(Feedback, true);
+			
+			if(Debug.isDebugBuild)
+				Debug.LogWarning (www.error + ": " + www.text);
+		}
+	}
+
+	public void FetchAddressData(){
+		Debug.Log("Getting data");
+	}
+
+	public void GenerateForm(){
 		form = new WWWForm();
 
-		if(newRegisterDetails.sex == "Male")
-			form.AddField("male", "1");
-		else
-			form.AddField("female", "1");
-
+//		if(newRegisterDetails.sex == "Male"){
+//			form.AddField("male", "1");
+//		} else {
+//			form.AddField("female", "1");
+//		}
+		form.AddField("male", newRegisterDetails.male);
 		form.AddField("dob", newRegisterDetails.dob);
 		form.AddField("firstname", newRegisterDetails.firstname);
 		form.AddField("lastname", newRegisterDetails.lastname);
 		form.AddField("school", newRegisterDetails.school);
 		form.AddField("address", newRegisterDetails.address);
-		form.AddField("address_suburb", newRegisterDetails.suburb);
-		form.AddField("address_state", newRegisterDetails.state);
-		form.AddField("address_postcode", newRegisterDetails.postcode);
+		form.AddField("address_suburb", newRegisterDetails.address_suburb);
+		form.AddField("address_state", newRegisterDetails.address_state);
+		form.AddField("address_postcode", newRegisterDetails.address_postcode);
 		form.AddField("phone", newRegisterDetails.phone);
 		form.AddField("email", newRegisterDetails.email);
 		form.AddField("password", newRegisterDetails.password);
@@ -160,10 +220,16 @@ public class LoginManager : MonoBehaviour {
 		form.AddField("schoolcode", "");
 		form.AddField("teacher", "wow");
 
-		jsonString = JsonUtility.ToJson(newRegisterDetails);
-//		Debug.Log(jsonString);
+	}
 
-		StartCoroutine(Register());
+	public void FormatDOB(){
+		if(dobGO.GetComponent<InputField>().text.Length == 2)
+			dobGO.GetComponent<InputField>().text += "-";
+	}
+
+	public void FormatDOBonEnd(){
+		dobGO.GetComponent<InputField>().text = dobGO.GetComponent<InputField>().text.Insert(4, "-");
+		dobGO.GetComponent<InputField>().text = dobGO.GetComponent<InputField>().text.Insert(7, "-");
 	}
 	
 	public void ResetFeedback(){
@@ -172,20 +238,29 @@ public class LoginManager : MonoBehaviour {
 	}
 
 	public void PopulateRegistration(){
-		newRegisterDetails.sex = sexGO.GetComponent<ToggleGroup>().ActiveToggles().First().gameObject.name;
+//		newRegisterDetails.sex = sexGO.GetComponent<ToggleGroup>().ActiveToggles().First().gameObject.name;
+		newRegisterDetails.male = maleGO.GetComponent<Toggle>().isOn ? "1" : "0";
 		newRegisterDetails.dob = dobGO.GetComponent<InputField>().text;
 		newRegisterDetails.firstname = firstnameGO.GetComponent<InputField>().text;
 		newRegisterDetails.lastname = lastnameGO.GetComponent<InputField>().text;
 		newRegisterDetails.school = schoolGO.GetComponent<InputField>().text;
 
 		newRegisterDetails.address = addressGO.GetComponent<InputField>().text;
-		newRegisterDetails.suburb = suburbGO.GetComponent<InputField>().text;
-		newRegisterDetails.state = stateGO.GetComponent<Dropdown>().captionText.text;
-		newRegisterDetails.postcode = postcodeGO.GetComponent<InputField>().text;
+		newRegisterDetails.address_suburb = suburbGO.GetComponent<InputField>().text;
+		newRegisterDetails.address_state = stateGO.GetComponent<Dropdown>().captionText.text;
+		newRegisterDetails.address_postcode = postcodeGO.GetComponent<InputField>().text;
 
 		newRegisterDetails.phone = phoneGO.GetComponent<InputField>().text;
 		newRegisterDetails.email = emailGO.GetComponent<InputField>().text;
 		newRegisterDetails.password = newPasswordGO.GetComponent<InputField>().text;
+
+		newRegisterDetails.schoolcode = "1";
+		newRegisterDetails.teacher = "t";
+		newRegisterDetails.guardian_firstname = "gfn";
+		newRegisterDetails.guardian_lastname = "gln";
+		newRegisterDetails.guardian_dob = "1982-11-22";
+		newRegisterDetails.guardian_title = "mr";
+		newRegisterDetails.relationship = "parent";
 	}
 	
 	//Feedback
@@ -234,56 +309,7 @@ public class LoginManager : MonoBehaviour {
 		PlayerPrefs.Save();
 	}
 
-	IEnumerator Register () {
-		if(debugLogin)
-			Debug.Log("Registering");
 
-//		Dictionary<string, string> headers = new Dictionary<string, string>();
-//		headers.Add("Content-Type", "application/json");
-//		headers.Add("Accept", "application/json");
-//		System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
-		byte[] json =  form.data;//encoding.GetBytes(jsonString);
-		
-		DisplayScreen(Feedback, false);
-		WWW www = new WWW(APIRegistration, json);
-		yield return www;
-
-		Debug.Log(www.error + www.text);
-//		string errorCode = "400";
-//		if(www.error == null)
-//			errorCode = "200";
-//		else
-//			if(www.error.Length >= 3)
-//				errorCode = www.error.Substring(0,3);
-//		
-//		if(Debug.isDebugBuild || debugLogin)
-//			Debug.Log("Answer from " + APIRegistration + ": " + errorCode + www.text);
-//		
-//		if (errorCode == "200") {
-//			List<object> users = MiniJSON.Json.Deserialize(www.text) as List<object>;
-//
-//			if(users != null){
-//
-//				if(debugLogin)
-//					Debug.Log("Loggedin with more than one kid");
-//
-//				foreach(Dictionary<string, object> user in users) {
-//					CreateUserFromDict(user);
-//				}
-//
-//			} else {
-//
-//				CreateUserFromAnswer(www.text);
-//			}
-//				
-//		} else {
-//		
-//			DisplayScreen(Feedback, true);
-//			
-//			if(Debug.isDebugBuild)
-//				Debug.LogWarning (www.error + ": " + www.text);
-//		}
-	}
 	
 	IEnumerator Login () {
 		if(debugLogin)
@@ -325,6 +351,7 @@ public class LoginManager : MonoBehaviour {
 			} else {
 
 				CreateUserFromAnswer(www.text);
+				loginGO.GetComponentInChildren<Image>().color = Color.green;
 			}
 				
 		} else {
@@ -341,7 +368,7 @@ public class LoginManager : MonoBehaviour {
 		newUser = gameObject.AddComponent<User>();
 		newUser.SetupUser((int)(long)answerDict["id"], (string)answerDict["email"], (string)answerDict["firstname"]);
 		SaveUser();
-		loginGO.GetComponentInChildren<Image>().color = Color.green;
+
 	}
 
 	public void CreateUserFromAnswer(string answer){
--- Assets/Scripts/RegisterDetails.cs
+++ Assets/Scripts/RegisterDetails.cs
@@ -4,15 +4,22 @@ using System.Collections.Generic;
 
 public class RegisterDetails : MonoBehaviour {
 
-	public string sex;
+	public string male;
 	public string dob;
 	public string firstname;
 	public string lastname;
 	public string school;
+	public string schoolcode;
+	public string teacher;
+	public string guardian_firstname;
+	public string guardian_lastname;
+	public string guardian_dob;
+	public string guardian_title;
+	public string relationship;
 	public string address;
-	public string suburb;
-	public string state;
-	public string postcode;
+	public string address_suburb;
+	public string address_state;
+	public string address_postcode;
 	public string phone;
 	public string email;
 	public string password;
@@ -28,10 +35,37 @@ public class RegisterDetails : MonoBehaviour {
 		}
 	}
 	
-	public RegisterDetails(string newSex, string newDob, string newFirstname, string newLastname, string newSchool, string newAddress, string newSuburb, string newState, string newPostcode, string newPhone, string newEmail, string newPassword){
-		firstname = newFirstname;
-		email = newEmail;
-		password = newPassword;
-	}
-
+	public RegisterDetails(
+		string newMale,
+		string newDob, 
+		string newFirstname, 
+		string newLastname, 
+		string newSchool, 
+		string newAddress, 
+		string newSuburb, 
+		string newState, 
+		string newPostcode, 
+		string newPhone, 
+		string newEmail, 
+		string newPassword){
+			male = newMale;
+			dob = newDob;
+			firstname = newFirstname;
+			lastname = newLastname;
+			school = newSchool;
+			address = newAddress;
+			address_suburb = newSuburb;
+			address_state = newState;
+			address_postcode = newPostcode;
+			phone = newPhone;
+			email = newEmail;
+			password = newPassword;
+			schoolcode = "1";
+			teacher = "t";
+			guardian_firstname = "gfn";
+			guardian_lastname = "gln";
+			guardian_dob = "1982-11-22";
+			guardian_title = "mr";
+			relationship = "parent";
+		}
 }
